5c731689f8ce23b4b6661d59e54908cadb033428
prernabansal984 <69748920+prernabansal984@users.noreply.github.com>
2021-05-29 00:41:58 +0530

    created by PRERNA BANSAL.

---
+++ mycaptain2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num1 , num2 , product;
+    cout<<"enter two numbers : ";
+    cin>>num1 >>num2;
+    product = num1 * num2;
+    cout<<"product = "<<product;
+}
